56af57829db588574150891eb06b73bdd532b1dc
JJ Lintol <jjlintol@JJs-MacBook-Pro.lan>
2023-12-25 15:10:53 -0500

    fix links on user card

--- components/card/UserCard.tsx
+++ components/card/UserCard.tsx
@@ -25,24 +25,27 @@ const UserCard = async ({
   });
 
   return (
-    <Link
-      href={`/profile/${clerkId}`}
-      className="shadow-light100_darknone w-full max-xs:min-w-full xs:w-[260px]"
-    >
+    <div className="shadow-light100_darknone w-full max-xs:min-w-full xs:w-[260px]">
       <article className="background-light900_dark200 light-border flex w-full flex-col items-center justify-center rounded-2xl border p-8">
-        <Image
-          src={picture}
-          width={100}
-          height={100}
-          className="rounded-full"
-          alt="profile"
-        />
-        <div className="mt-4 text-center">
-          <h3 className="h3-bold text-dark200_light900 line-clamp-1">{name}</h3>
-          <p className="body-regular text-dark500_light500 mt-2 line-clamp-1">
-            @{username}
-          </p>
-        </div>
+        <Link href={`/profile/${clerkId}`}>
+          <Image
+            src={picture}
+            width={100}
+            height={100}
+            className="rounded-full"
+            alt="profile"
+          />
+        </Link>
+        <Link href={`/profile/${clerkId}`}>
+          <div className="mt-4 text-center">
+            <h3 className="h3-bold text-dark200_light900 line-clamp-1">
+              {name}
+            </h3>
+            <p className="body-regular text-dark500_light500 mt-2 line-clamp-1">
+              @{username}
+            </p>
+          </div>
+        </Link>
         <div className="mt-5">
           {interactedTags.length > 0 ? (
             <div className="flex items-center gap-2">
@@ -55,7 +58,6 @@ const UserCard = async ({
                       ? `${tag.name.substring(0, 4)}..`
                       : tag.name
                   }
-                  clickable={false}
                 />
               ))}
             </div>
@@ -64,7 +66,7 @@ const UserCard = async ({
           )}
         </div>
       </article>
-    </Link>
+    </div>
   );
 };
 
--- components/shared/RenderTag.tsx
+++ components/shared/RenderTag.tsx
@@ -14,10 +14,8 @@ const RenderTag = ({
   _id,
   name,
   totalQuestions,
-  showCount,
-  clickable = true,
+  showCount
 }: IRenderTagProps) => {
-  if (clickable) {
     return (
       <Link href={`/tags/${_id}`} className="flex justify-between gap-2">
         <Badge className="subtle-medium background-light800_dark300 text-light400_light500 line-clamp-1 rounded-md border-none px-4 py-2 uppercase">
@@ -28,18 +26,6 @@ const RenderTag = ({
         )}
       </Link>
     );
-  } else {
-    return (
-      <div className="flex justify-between gap-2">
-        <Badge className="subtle-medium background-light800_dark300 text-light400_light500 line-clamp-1 rounded-md border-none px-4 py-2 uppercase">
-          {name}
-        </Badge>
-        {showCount && (
-          <p className="small-medium text-dark500_light700">{totalQuestions}</p>
-        )}
-      </div>
-    );
-  }
 };
 
 export default RenderTag;
